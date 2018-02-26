e9ae006bca5f7f30ed715ad20eda1a36b20cd1ac
ginamoller <ginamolleren@gmail.com>
2018-02-26 08:41:11 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,14 @@
+function startProgram(){
+  var variabel = "VARIABEL";
+
+  function test1(){
+    for(variabel = 0; variabel < 2; variabel++){
+      console.log(variabel);
+    }
+  console.log(variabel);
+  }
+
+function logVariabel(){
+  console.log("Verdi nå:", variabel);
+}
+}
